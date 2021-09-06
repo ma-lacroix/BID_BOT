7e516d5f58da9a1833cf2f506688bcf8888b75fa
ma-lacroix <lacroix.marcantoine@gmail.com>
2021-09-06 23:04:02 +0200

    Second cpp modifications

--- src/sharpe.cpp
+++ src/sharpe.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include <vector>
-#include <string>
+#include <ctime>
 
 std::vector<float> gen_random_weights(int simulations){
-    
+// generates random portfolio allocation weights
     std::vector<float> weights;
-    srand((unsigned)time(NULL));
     
     for(size_t i {0};i < simulations; ++i){
         int a = std::rand() % 100 + 1;
@@ -22,7 +21,19 @@ std::vector<float> gen_random_weights(int simulations){
     return weights;
 }
 
-std::vector<float> get_sharpe_ratios(int simulations){
+float get_return(std::vector<float> weights, std::vector<float> log_returns_means){
+// returns expected ROI given security weights
+    float total {0};
+    for(size_t i {0};i<log_returns_means.size();++i){
+        total+=weights.at(i)*log_returns_means.at(i);
+    }
+    total = total*log_returns_means.size();
+    std::cout << total << std::endl;
+    return total;
+}
+
+std::vector<float> get_sharpe_ratios(int simulations, std::vector<float> log_returns_means,
+                                    std::vector<float> log_returns_cov){
     /* PYTHON 
     print("\nGetting Sharpe ratios...\n")
     all_weights = np.zeros((simulations,len(df.columns)))
@@ -48,8 +59,8 @@ std::vector<float> get_sharpe_ratios(int simulations){
     for(size_t ind {0}; ind < simulations; ++ind){
         std::vector<float> weights = gen_random_weights(simulations);
         all_weights.push_back(weights);
-        ret_arr;
-        vol_arr;
+        ret_arr.push_back(get_return(weights,log_returns_means));
+        // vol_arr;
         sharpe_arr;
     }
 
@@ -58,6 +69,9 @@ std::vector<float> get_sharpe_ratios(int simulations){
 }
 
 int main(){
-    get_sharpe_ratios(10); // 10 just a placeholder
+    std::vector<float> dummy_returns {10.0,0.5,0.5,0.23,0.1};
+    std::vector<float> dummy_cov {};
+    std::vector<float> sharpe_arr {};
+    sharpe_arr = get_sharpe_ratios(5,dummy_returns); // 10 just a placeholder
     return 0;
 }
