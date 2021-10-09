7816503be4cfecb0eb3416765673b66a95b38a14
ma-lacroix <lacroix.marcantoine@gmail.com>
2021-10-09 23:04:16 +0200

    Minor code refactoring

--- src/gcp.py
+++ src/gcp.py
@@ -9,14 +9,14 @@ key = 'key/key.json'
 
 def get_project():
 # hide projectId
-    with open('key/key.json') as f:
+    with open(key) as f:
         projectId = json.load(f)['project_id']
         print(projectId)
     f.close()
     return projectId
 
 def get_client():
-    client = service_account.Credentials.from_service_account_file('key/key.json')
+    client = service_account.Credentials.from_service_account_file(key)
     return client
 
 def read_csv(projectId,csv,client):
@@ -39,22 +39,25 @@ WITH r AS (SELECT
   WHERE yyyy_mm_dd = (
     SELECT recent FROM r
     )
+  GROUP BY 1,2,3,4,5,6,7,8,9,10
   )
-SELECT * FROM data  
+SELECT * FROM data WHERE Share > 0
     '''
     try:
         print("Trying query...")
+        bigquery.QueryJobConfig
         query_job = client.query(query, job_config=config)  # Make an API request.
         query_job.result()  # Wait for the job to complete.
     except:
         print("Query failed; trying again...")
-        time.sleep(10)
+        time.sleep(5)
         submit_job(client,config,projectId)
 
 
 def update_recent(projectId):
-    client = bigquery.Client.from_service_account_json('key/key.json')
-    config = bigquery.QueryJobConfig(destination=f'{projectId}.bid_bot.most_recent')
+    client = bigquery.Client.from_service_account_json(key)
+    config = bigquery.QueryJobConfig(destination=f'{projectId}.bid_bot.most_recent',
+                                        write_disposition='WRITE_TRUNCATE')
     submit_job(client,config,projectId)
 
 
