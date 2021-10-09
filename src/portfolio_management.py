2d2ec4dd05126086ed542a0d21d3424d22d788d9
ma-lacroix <lacroix.marcantoine@gmail.com>
2021-10-09 21:42:16 +0200

    Minor DF change

--- src/portfolio_management.py
+++ src/portfolio_management.py
@@ -43,23 +43,25 @@ class Portfolio:
                 gains_6m.append(0)
                 gains_3m.append(0)
                 gains_1m.append(0)
+        self.df['Sector'] = self.sector
         self.df['total_price'] = np.round(total_price,2)
         self.df['total_stocks'] = np.round(total_stocks,0)
         self.df['exp_gains_3m'] = np.round(gains_3m,2)
         self.df['exp_gains_1m'] = np.round(gains_1m,2)
-        totals = {'yyyy_mm_dd':self.df['yyyy_mm_dd'][0],'Symbol':'TOTAL','Share':1.0,'Close':np.sum(self.df['Close']),
-                    'Close3m':np.sum(self.df['Close']),'Name':'TOTAL','total_price':np.sum(self.df['total_price']),
+        totals = {'Sector':self.sector,'yyyy_mm_dd':self.df['yyyy_mm_dd'][0],'Symbol':'TOTAL'.format(self.sector),
+                    'Share':1.0,'Close':np.sum(self.df['Close']),'Close3m':np.sum(self.df['Close']),
+                    'Name':'TOTAL','total_price':np.sum(self.df['total_price']),
                     'total_stocks':np.sum(self.df['total_stocks']),'exp_gains_3m':np.sum(self.df['exp_gains_3m']),
                     'exp_gains_1m':np.sum(self.df['exp_gains_1m'])}
-        self.df = self.df.append(totals,ignore_index=True)[['yyyy_mm_dd','Symbol','Name','Share','Close','total_price',
+        self.df = self.df.append(totals,ignore_index=True)[['Sector','yyyy_mm_dd','Symbol','Name','Share','Close','total_price',
                                                             'total_stocks','exp_gains_3m','exp_gains_1m']]
         self.df.to_csv(self.csv_file,index=False) # overwrite results file            
         print(self.df)
 
     def trigger_update(self):
-        utils.update(self.sector,self.maxPrice)
+        # utils.update(self.sector,self.maxPrice)
         self.init_csv_file()
-        self.init_df()
-        self.get_shares()
+        # self.init_df()
+        # self.get_shares()
         gcp.trigger_upload(self.csv_file)
         
